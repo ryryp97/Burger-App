9afe63b97dfd90fc87e0d7526016be57356f59f9
ryry <ryanpetrecca@gmail.com>
2021-01-23 15:54:44 -0800

    added the skeleton for the orm and linked in to burger.js

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+const connection = require("../config/connection.js");
+
+const orm = {
+    selectAll: () => {
+
+    },
+
+    insertOne: () => {
+
+    },
+
+    updateOne: () => {
+
+    },
+
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
