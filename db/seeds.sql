3084807e09fb137ebf102eab1e22aa7be3709d8d
ryry <ryanpetrecca@gmail.com>
2021-01-23 15:42:32 -0800

    added a schema and seeds file for the database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('double-double');
+INSERT INTO burgers (burger_name) VALUES ('bacon cheeseburger');
