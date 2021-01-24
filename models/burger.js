bda3c69eec7065c5720b684fdb0d777ffad299a3
ryry <ryanpetrecca@gmail.com>
2021-01-23 16:00:08 -0800

    added the skeleton to the burger.js file and exported it

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,18 @@
-var orm = require("../config/orm.js");
+const orm = require("../config/orm.js");
 
+const burgers = {
+    selectAll: () => {
+        orm.selectAll('burgers', )
+    },
+
+    insertOne: () => {
+        orm.insertOne('burgers', )
+    },
+
+    updateOne: () => {
+        orm.updateOne('burgers', )
+    },
+
+}
+
+module.exports = burgers;
