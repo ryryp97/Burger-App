7ca1521245880fd88496925c2f89736f45da6145
ryry <ryanpetrecca@gmail.com>
2021-01-29 17:14:13 -0800

    minor changes

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 const connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
